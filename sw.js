6660b70a6bdc779b1b2c14d345d956785fdb3e10
nurqamar1995-dotcom <nurqamar1995@gmail.com>
2026-08-22 12:16:03 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'retail-assistant-v1';
+const urlsToCache = [
+    'index.html',
+    'manifest.json',
+    'kalkulator_sales.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
