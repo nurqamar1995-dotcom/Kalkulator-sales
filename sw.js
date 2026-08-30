b2a52b91d4cabc510ef15ad75343f94f8186b290
nurqamar1995-dotcom <nurqamar1995@gmail.com>
2026-08-30 20:38:39 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'retail-assistant-v5.0'; // Versi dinaikkan agar HP otomatis sadar ada update
+const CACHE_NAME = 'retail-assistant-v5.1'; // Versi dinaikkan agar HP otomatis sadar ada update
 
 const urlsToCache = [
     '/',
